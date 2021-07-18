0ba88d7987c429e525f1254c4b34533332910df0
June Arness <junior@nationwidesoftware.com>
2021-07-18 09:55:29 -0400

    changes

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+var class;
+var work;
+<button onclick="JS.getElementById('demo').innerHTML = Date()">The time is?</button>
+var btn = JS.createElement("button")
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
