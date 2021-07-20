9ddf4b17c2c8bcb3369153efeb3b686a68fcccee
June Arness <junior@nationwidesoftware.com>
2021-07-20 19:06:14 -0400

    changes

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -3,5 +3,5 @@ window.alert("Get it done") // Text to be displayed
 let str1 = "jax";  // Name of string 1
 let str2 = "sonja"; //Name of string 2
 let res = str1.concat(str2) //string 1 and string 2 concated
-String.fontcolor("red") // font color
+fontcolor("red") // font color
 var patt = /mortalkombat/i // variable expression.
