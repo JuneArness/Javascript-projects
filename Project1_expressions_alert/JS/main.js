1f852ef64267b57bd1a12aa50ee137269d5dd302
June Arness <junior@nationwidesoftware.com>
2021-07-20 20:19:49 -0400

    changes

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -3,5 +3,15 @@ window.alert("Get it done"); // Text to be displayed
 let str1 = "jax";  // Name of string 1
 let str2 = "sonja"; //Name of string 2
 let res = str1.concat(str2); //string 1 and string 2 concated
-var result = str.fontcolor("red"); // font color
+function textcolor() {
+    var result = str1.fontcolor("red"); // font color
+    document.getElementById("color").innerHTML=result;
+
+}
+function concated() {
+    let res = str1.concat(str2); //string 1 and string 2 concated
+    document.getElementById("concat").innerHTML=res;
+
+}
+
 var patt = /mortalkombat/i; // variable expression.
--- project6_example/JS/Example.js
+++ project6_example/JS/Example.js
@@ -1,3 +1,7 @@
 function my_Function() {
     document.getElementById("Test").innerHTML = 0/0;
+    document.getElementById("Test").innerHTML = isNaN('This is a string');
+    document.getElementById("Test").innerHTML = isNaN('007');
+    document.write(2E310);
+    document.write(-3E310);
 }
