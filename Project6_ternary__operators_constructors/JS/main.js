a6a8cedea99533e7d25cdbed23a6e98c5a26bdb7
June Arness <junior@nationwidesoftware.com>
2021-07-22 08:38:08 -0400

    changes

---
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,7 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("RIde").innerHTML = Can_ride = " to ride.";
+    
+}
