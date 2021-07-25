d7fde3519ab90acf965a9a6157051845a2126714
June Arness <junior@nationwidesoftware.com>
2021-07-24 23:23:06 -0400

    changes

--- Project6_ternary__operators_constructors/JS/main.js
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -1,3 +1,22 @@
+document.write(Bigger = (5 > 1) ? "Left number is bigger":"Right number is bigger");
+
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride";
+}
+
+class Customer
+    {
+        string David Banner;
+        Boolean Active;
+        Customer(string David Banner){
+            Fullname = David Banner;
+            Active = true
+        }
+
+    }
 
 function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
@@ -6,36 +25,24 @@ function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Color = Color;
 }
 
-var Jack = new Vehicle("Dodge", "Viper" , 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+var Jack = new Vehicle("Dodge", "Viper", "2020", "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk","2019","White and Black");
+var Erik = new Vehicle("Ford","Pinto",1971,"Mustard");
 function myFunction() {
-    document.getElementById("keywords_and_Constructors").innerHTML = 
-    "Erik drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model + "manufactured in " + Erik.Vehicle_Year;
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    "manufactured in " + Erik.Vehicle_Year;
 }
 
-function Person(first, last, age) {
-    this.firstName=first;
-    this.lastName=last;
-    this.age=age;
-}
+var X = 10;
+var Y = "Charlie"
 
-var new_Person=new Person("David","Banner",18);
-function count_Function () {
-    document.getElementById("Counting").innerHTML = Count ();
-   
-    
-}
-function Count() {
-    var Starting_point = 9;
-    function Plus_one() {Starting_point += 1;}
-    Plus_one();
-    return Starting_point;
-}
-function Vote_Function() {
-    var Age, Can_vote;
-    Age = document.getElementById("Age").value;
-    Can_vote = (Age < 18) ? "You are not eligible to vote":"You can vote";
-    document.getElementById("Vote").innerHTML = Can_vote = " to vote.";
-    
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point+= 1;}
+        Plus_one();
+        return Starting_point;
+    }
 }
