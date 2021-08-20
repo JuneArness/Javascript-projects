b564453017f839519645465996c12cee51ee4690
JuneArness <junior@nationwidesoftware.com>
2021-08-20 16:28:23 -0400

    Add files via upload

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -19,24 +19,26 @@ SELECT * FROM tbl_class;
 
 
 CREATE TABLE tbl_culture (
-culture_id INT FOREIGN KEY NOT NULL IDENTITY (100,1),
-culture_type VARCHAR(50) NOT NULL
+culture_id INT PRIMARY KEY NOT NULL IDENTITY (100,1),
+culture_type VARCHAR(50) NOT NULL,
+class_type INT FOREIGN KEY REFERENCES tbl_class(class_id)
 );
 
 INSERT INTO tbl_culture
-    (culture_type)
+    (culture_type, class_type)
 	VALUES
-	('hopper'),
-	('dancer'),
-	('best paint'),
-	('best setup'),
-	('best audio'),
-	('car clubs'),
-	('best in show'),
-	('classic')
+	('hopper', 2),
+	('dancer', 2),
+	('best paint', 1),
+	('best setup', 1),
+	('best audio', 1),
+	('car clubs', 1),
+	('best in show', 1),
+	('classic', 2)
 	;
 
-	
+	SELECT * FROM tbl_class
+	INNER JOIN tbl_culture ON tbl_class.class_id = tbl_culture.class_type
 
 	
 
